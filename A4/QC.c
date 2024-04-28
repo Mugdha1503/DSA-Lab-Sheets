37fcb52e9c9757d40b0ffd8cbce73142d48442ec
Mugdha Gupta <111490641+Mugdha1503@users.noreply.github.com>
2024-04-28 11:54:41 +0530

    Update QC.c

--- A4/QC.c
+++ A4/QC.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
-int maxSumOfRatings(int n, int k, int ratings[], int time[]) {
-    int dp[n + 1][k + 1];
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= k; j++) {
-            if (i == 0 || j == 0)
-                dp[i][j] = 0;
-            else if (time[i - 1] <= j)
-                dp[i][j] = (ratings[i - 1] + dp[i - 1][j - time[i - 1]]) > dp[i - 1][j] ?
-                           (ratings[i - 1] + dp[i - 1][j - time[i - 1]]) : dp[i - 1][j];
-            else
-                dp[i][j] = dp[i - 1][j];
-        }
+int knapsack(int r[], int t[], int indx, int k)
+{
+    if (indx == 0)
+    {
+        if (t[indx] <= k)
+            return r[indx];
+        return 0;
     }
-    return dp[n][k];
+    int notTaken = knapsack(r, t, indx - 1, k);
+    int taken = 0;
+    if (t[indx] <= k)
+        taken = r[indx] + knapsack(r, t, indx - 1, k - t[indx]);
+    return (notTaken > taken) ? notTaken : taken;
 }
-int main() {
+int main()
+{
     int n, k;
     scanf("%d %d", &n, &k);
-    int ratings[n], time[n];
+    int ratings[n];
     for (int i = 0; i < n; i++)
         scanf("%d", &ratings[i]);
+    int time[n];
     for (int i = 0; i < n; i++)
         scanf("%d", &time[i]);
-    int maxSum = maxSumOfRatings(n, k, ratings, time);
-    printf("%d\n", maxSum);
-    return 0;
+    printf("%d", knapsack(ratings, time, n - 1, k));
 }
-
-
